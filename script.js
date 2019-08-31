ad15de5c5b365c8793208be156e31ea923deda91
Armia Afridi <49521014+Armia13@users.noreply.github.com>
2019-08-31 09:08:39 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 
-// let hours = 0
-// let minutes = 0
-// let seconds = 0
-// let timer
-// let isRunning = false
+let hours = 0
+let minutes = 0
+let seconds = 0
+let timer
+let isRunning = false
 
-// function updateTime () {
-//   if (seconds == 59) {
-//     seconds = 0
-//     if (minutes === 59) {
-//       minutes = 0
-//       hours++
-//     } else {
-//       minutes++
-//     }
-//   } else {
-//     seconds++
-//   }
-//   document.getElementById('seconds').innerHTML = seconds;
-//   document.getElementById('minutes').innerHTML = minutes;
-//   document.getElementById('hours').innerHTML = hours;
-// }
+function updateTime () {
+  if (seconds == 59) {
+    seconds = 0
+    if (minutes === 59) {
+      minutes = 0
+      hours++
+    } else {
+      minutes++
+    }
+  } else {
+    seconds++
+  }
+  document.getElementById('seconds').innerHTML = seconds;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('hours').innerHTML = hours;
+}
 
-// function startTime () {
-//   if (isRunning === false) {
-//     timer = setInterval(updateTime, 1000)
-//     isRunning = true
-//   }
-// }
+function startTime () {
+  if (isRunning === false) {
+    timer = setInterval(updateTime, 1000)
+    isRunning = true
+  }
+}
 
-// function stopTime () {
-//   clearInterval(timer)
-//   isRunning = false
-// }
+function stopTime () {
+  clearInterval(timer)
+  isRunning = false
+}
 
-// function resetTime () {
-//   hours = 0
-//   minutes = 0
-//   seconds = 0
-//   document.getElementById('seconds').innerHTML = seconds;
-//   document.getElementById('minutes').innerHTML = minutes;
-//   document.getElementById('hours').innerHTML = hours;
-//   stopTime()
-// }
+function resetTime () {
+  hours = 0
+  minutes = 0
+  seconds = 0
+  document.getElementById('seconds').innerHTML = seconds;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('hours').innerHTML = hours;
+  stopTime()
+}
 
